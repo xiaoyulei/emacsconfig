ebb903ec79bf1500cd4b14544ce598a580b74e5e
yuleixiao <21739034@qq.com>
2015-09-30 09:26:04 +0800

    update

---
+++ elpa/gotest-20150917.2345/gotest-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gotest" "20150917.2345" "Launch GO unit tests" '((emacs "24.3") (s "1.9.0") (f "0.17.2") (go-mode "1.0.0")) :url "https://github.com/nlamirault/gotest.el" :keywords '("languages" "go" "tests"))
