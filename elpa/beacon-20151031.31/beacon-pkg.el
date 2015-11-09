92212dbaf2f1af04cab70553de8afb8aab3bfd1a
yuleixiao <21739034@qq.com>
2015-11-09 09:38:53 +0800

    update

---
+++ elpa/beacon-20151031.31/beacon-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "beacon" "20151031.31" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
---
+++ elpa/guru-mode-20151027.2228/guru-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "guru-mode" "20151027.2228" "Become an Emacs guru" 'nil :url "https://github.com/bbatsov/guru-mode" :keywords '("convenience"))
