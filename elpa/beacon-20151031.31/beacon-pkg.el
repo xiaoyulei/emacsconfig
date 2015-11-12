4b09c7461d47c4bec7818d12fd7d6227fc7a304e
yuleixiao <21739034@qq.com>
2015-11-12 16:45:50 +0800

    update

--- elpa/beacon-20151031.31/beacon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beacon" "20151031.31" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
