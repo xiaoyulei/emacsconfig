971f9c3437376f73c5fc5bd98933ed25f44a9542
yuleixiao <21739034@qq.com>
2015-12-15 10:09:58 +0800

    update

--- elpa/beacon-20151111.826/beacon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beacon" "20151111.826" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
--- elpa/smex-20150822.1146/smex-autoloads.el
+++
@@ -1,35 +0,0 @@
-;;; smex-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "smex" "smex.el" (21979 14718 78054 68000))
-;;; Generated autoloads from smex.el
-
-(autoload 'smex "smex" "\
-
-
-\(fn)" t nil)
-
-(autoload 'smex-major-mode-commands "smex" "\
-Like `smex', but limited to commands that are relevant to the active major mode.
-
-\(fn)" t nil)
-
-(autoload 'smex-initialize "smex" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("smex-pkg.el") (21979 14718 185329 570000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; smex-autoloads.el ends here
