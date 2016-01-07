8e8a165224aeebda55b33075d99a754c1037f7f7
yuleixiao <21739034@qq.com>
2016-01-07 11:32:04 +0800

    update

--- elpa/go-projectile-20151208.1029/go-projectile-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; go-projectile-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("go-projectile.el") (22127 30283 927298
-;;;;;;  999000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; go-projectile-autoloads.el ends here
--- elpa/yaml-mode-20150816.2235/yaml-mode-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; yaml-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "yaml-mode" "yaml-mode.el" (21979 14714 46054
-;;;;;;  90000))
-;;; Generated autoloads from yaml-mode.el
-
-(let ((loads (get 'yaml 'custom-loads))) (if (member '"yaml-mode" loads) nil (put 'yaml 'custom-loads (cons '"yaml-mode" loads))))
-
-(autoload 'yaml-mode "yaml-mode" "\
-Simple mode to edit YAML.
-
-\\{yaml-mode-map}
-
-\(fn)" t nil)
-
-(add-to-list 'auto-mode-alist '("\\.e?ya?ml$" . yaml-mode))
-
-;;;***
-
-;;;### (autoloads nil nil ("yaml-mode-pkg.el") (21979 14714 141196
-;;;;;;  67000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; yaml-mode-autoloads.el ends here
