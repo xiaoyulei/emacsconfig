f06e5f045dcf485dd451d802dc5ddf2def4d0315
yuleixiao <21739034@qq.com>
2016-01-18 11:51:56 +0800

    update

--- elpa/beacon-20151205.1003/beacon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beacon" "20151205.1003" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
--- elpa/zenburn-theme-20160103.1612/zenburn-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "zenburn-theme" "20160103.1612" "A low contrast color theme for Emacs." 'nil :url "http://github.com/bbatsov/zenburn-emacs")
