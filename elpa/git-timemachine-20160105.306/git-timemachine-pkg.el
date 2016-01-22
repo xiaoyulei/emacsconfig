39ac6189b2da14631be20830328a44656112470e
yuleixiao <21739034@qq.com>
2016-01-22 16:32:44 +0800

    update

--- elpa/git-timemachine-20160105.306/git-timemachine-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-timemachine" "20160105.306" "Walk through git revisions of a file" 'nil :url "https://github.com/pidu/git-timemachine" :keywords '("git"))
