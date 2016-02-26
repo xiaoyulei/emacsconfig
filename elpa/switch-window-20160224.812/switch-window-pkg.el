2435c8548e5157d981fa8b020910169f7640483a
yuleixiao <21739034@qq.com>
2016-02-26 14:16:05 +0800

    update

---
+++ elpa/switch-window-20160224.812/switch-window-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "switch-window" "20160224.812" "A *visual* way to choose a window to switch to" '((cl-lib "0.5")) :url "https://github.com/dimitri/switch-window" :keywords '("window" "navigation"))
