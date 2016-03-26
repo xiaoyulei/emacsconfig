90816af6b9265fff75aa519251edce51e0a95407
yuleixiao <21739034@qq.com>
2016-03-26 09:23:06 +0800

    update

--- elpa/beacon-20160206.917/beacon-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "beacon" "20160206.917" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
---
+++ elpa/beacon-20160321.551/beacon-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "beacon" "20160321.551" "Highlight the cursor whenever the window scrolls" '((seq "1.11")) :url "https://github.com/Malabarba/beacon" :keywords '("convenience"))
