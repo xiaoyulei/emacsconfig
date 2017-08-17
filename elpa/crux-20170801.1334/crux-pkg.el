b06f7db2ed4ebce812bcde459c9c7647810d28c2
yuleixiao <21739034@qq.com>
2017-08-17 10:22:32 +0800

    update

---
+++ elpa/crux-20170801.1334/crux-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "crux" "20170801.1334" "A Collection of Ridiculously Useful eXtensions" '((seq "1.11")) :commit "4f5c8fefd5a6aa52e128c4a0401cc86410d6ac8f" :url "https://github.com/bbatsov/crux" :keywords '("convenience"))
---
+++ elpa/gotest-20170811.0/gotest-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gotest" "20170811.0" "Launch GO unit tests" '((emacs "24.3") (s "1.11.0") (f "0.19.0") (go-mode "1.4.0")) :commit "22f54d6c00c48f38a04a74990db501946405c1a8" :url "https://github.com/nlamirault/gotest.el" :keywords '("languages" "go" "tests"))
