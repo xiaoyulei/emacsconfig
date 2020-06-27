790dc20a00ca8a63d40b5ecc5a82891db38b3411
xiaoyulei <12639880+xiaoyulei@users.noreply.github.com>
2020-06-27 13:12:45 +0300

    Update init.el

--- init.el
+++ init.el
@@ -39,6 +39,17 @@
 ;; You may delete these explanatory comments.
 ;(package-initialize)
 
+;; proxy
+;(setq url-proxy-services
+;   '(("no_proxy" . "^\\(localhost\\|10\\..*\\|192\\.168\\..*\\)")
+;     ("http" . "proxy.hhhh.com:8080")
+;     ("https" . "proxy.hhhh.com:8080")))
+
+;(setq url-http-proxy-basic-auth-storage
+;   (list (list "proxy.hhhhh.com:8080"
+;         (cons "Input your LDAP UID !"
+;         (base64-encode-string "USERNAME:PASSWORD")))))
+
 (defvar current-user
   (getenv
    (if (equal system-type 'windows-nt) "USERNAME" "USER")))
